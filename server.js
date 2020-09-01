8c50dbf5f3eb0f86edfa8a3e2aaddd3eef106d45
robmitch802 <60521912+robmitch802@users.noreply.github.com>
2020-09-01 13:42:16 -0400

    server side started

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const path = require('path');
 const generatePassword = require('password-generator');
 const cors = require('cors');
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const router = express.Router();
+const multer = require('multer');
 
 require('dotenv').config();
 
@@ -11,6 +14,9 @@ const app = express();
 
 //serves static files from React app
 app.use(express.static(path.join(__dirname, 'client/build')))
+
+//defines directory for image uploads
+app.use(express.static('uploads'));
 
 //use bodyParser to support app/json post data
 app.use(bodyParser.json());
@@ -42,8 +48,45 @@ app.get('/api/passwords', (req, res) => {
 
 });
 
+//set up route for image uploads
+app.use('/api/images', require('/server-side/routes/api/images'));
+
+//sets up get action for images
+router.get('/', (req, res) => {
+    //finds directory and stores it
+    const uploadsDirectory=path.join('uploads');
+    //reads files in the directory
+    fs.readdir(uploadsDirectory, (err, files) => {
+        if (err) {
+            return res.json('Error: '+ err)
+        }
+        //if no files in directory, return message
+        if (files.length === 0) {
+            return res.json('No Images Uploaded')
+        }
+        //returns array of filenames in uploads directory
+        return res.json({ files })
+    })
+});
+
+//set up file placement using multer
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
+
+const uploads = multer({ storage })
+
+//router posting method for image uploads
+router.post('/', uploads.single('image'), async (req, res) => {
+    const image = req.file.path;
+    res.json('Image uploaded successfully!' + image)
+})
+
+
 //set up route for posts
-const postRouter = require('./server-side/routes/posts');
+const postRouter = require('/server-side/routes/posts');
 
 const getPost = async (req, res) => {
     console.log('getting post request');
